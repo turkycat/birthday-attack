238c522e2741b8af7c9625ce6dc7e2936dc7733a
Jesse Frush <jesse.frush@gmail.com>
2021-11-19 22:35:04 -0800

    now able to print the non-coinbase transactions from testnet blocks

--- utxo-build.py
+++ utxo-build.py
@@ -1,19 +1,48 @@
+from json import decoder
 from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 # rpcuser and rpcpassword are set in the bitcoin.conf file
 rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('turkyrpc', 'turkypass'))
+logfile = open("logfile.txt", "a")
+logfile.write("---------------------------------------------------------------------------\n")
+logfile.write("                           new session                                     \n")
+logfile.write("---------------------------------------------------------------------------\n")
 
 best_block_hash = rpc_connection.getbestblockhash()
 best_block = rpc_connection.getblock(best_block_hash)
 best_block_height = int(best_block["height"])
 
-#print(best_block)
-#print(best_block_height)
+# this was me remembering how to python, but leaving it for fun
+def print_last_20_block_hashes():
+    for i in range(best_block_height - 20, best_block_height):
+        block_hash = rpc_connection.getblockhash(i)
+        block = rpc_connection.getblock(block_hash)
+        print(block["hash"])
 
-for i in range(best_block_height - 20, best_block_height):
+def lw(message):
+    print(message)
+    logfile.write(message)
+    logfile.write("\n")
+
+utxo_set = {}
+def process_transactions(transactions, block_hash):
+    for i in range(1, len(transactions)):
+        tx_hash = transactions[i]
+        lw(str.format("getrawtransaction {0} 1 {1}", tx_hash, block_hash))
+        raw_tx = rpc_connection.getrawtransaction(tx_hash, True, block_hash)
+        lw(str(raw_tx))
+
+for i in range(400, 500):
     block_hash = rpc_connection.getblockhash(i)
     block = rpc_connection.getblock(block_hash)
-    print(block["hash"])
+    transactions = block["tx"]
+    if len(transactions) == 1:
+        lw("skipping block with only coinbase")
+        continue
+
+    process_transactions(transactions, block_hash)
+    
+    
 
 
 
