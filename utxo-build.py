8b619ff7fde3f680810caf61d52b59da32cf9fcb
Jesse Frush <jesse.frush@gmail.com>
2021-11-21 21:10:13 -0800

    now building dict of utxos and printing them

--- utxo-build.py
+++ utxo-build.py
@@ -19,20 +19,41 @@ def print_last_20_block_hashes():
         block = rpc_connection.getblock(block_hash)
         print(block["hash"])
 
+# TODO add loglevels or replace with a library
 def lw(message):
     print(message)
     logfile.write(message)
     logfile.write("\n")
 
+# utxo_set is currently K: tuple( transaction_hash, index ) V: script_pubkey
+# TODO: write a class object to encapsulate all the interesting information from unspent outputs 
 utxo_set = {}
 def process_transactions(transactions, block_hash):
     for i in range(1, len(transactions)):
-        tx_hash = transactions[i]
-        lw(str.format("getrawtransaction {0} 1 {1}", tx_hash, block_hash))
-        raw_tx = rpc_connection.getrawtransaction(tx_hash, True, block_hash)
-        lw(str(raw_tx))
+        txid = transactions[i]
+        lw(f"getrawtransaction {txid} 1 {block_hash}")
+        raw_tx = rpc_connection.getrawtransaction(txid, True, block_hash)
+        
+        for input in raw_tx["vin"]:
+            spent_output_key = (input["txid"], input["vout"])
 
-for i in range(400, 500):
+            # remove the spent output from our UTXO set
+            # one might think that the set must always contain the item-
+            # but this isn't true. an input could be a coinbase transaction.
+            # coinbase transactions are not retrievable via getrawtransaction
+            if spent_output_key in utxo_set:
+                lw(f"removing spent output with key: {spent_output_key}")
+                utxo_set.pop(spent_output_key)
+
+        for output in raw_tx["vout"]:
+            new_output_key = (txid, output["n"])
+            lw(f"adding new output with key: {new_output_key}")
+            utxo_set[new_output_key] = output["scriptPubKey"]
+
+# using the temporary range 500, 500 while building, eventually we will want to
+# periodically check the best block hash and get all blocks up to the current height
+for i in range(499, 500):
+# for i in range(0, best_block_height):
     block_hash = rpc_connection.getblockhash(i)
     block = rpc_connection.getblock(block_hash)
     transactions = block["tx"]
@@ -42,7 +63,17 @@ for i in range(400, 500):
 
     process_transactions(transactions, block_hash)
     
+def print_utxo_set():
+    print("[")
+    for item in utxo_set.items():
+        txid, index = item[0]
+        script_pubkey = item[1]
+        print(f"{{'txid': '{txid}','index': '{index}', 'script_pubkey': {script_pubkey}}},")
+    print("]")
     
+
+print_utxo_set()
+
 
 
 
