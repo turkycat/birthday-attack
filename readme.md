6a4aa8b286c76570da66fc379f653f6371bdb4f3
Jesse Frush <jesse.frush@gmail.com>
2021-11-24 14:25:36 -0800

    blocks are now retrieved individually from a batch of transactions, fixed RPC timeout issue

--- readme.md
+++ readme.md
@@ -20,6 +20,8 @@ https://github.com/rsalmei/alive-progress
 
 ## setup
 
+set `bitcoin.conf` property `rpcservertimeout=1200` to guarantee that the RPC connection doesn't get terminated early. if this is undesirable or impossible, the `utxo_build.py` should be modified to reconnect when `IOError` is caught or reconnect before each new batch of block hashes is retrieved as default timeouts period of 30s can occur when saving/loading progress.
+
 ```bash
 pip install python-bitcoinrpc
 pip install alive-progress
--- utxo_build.py
+++ utxo_build.py
@@ -17,10 +17,10 @@ file_format = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(mess
 log = logging.getLogger(__name__)
 log.setLevel(logging.INFO)
 
-# info_handler = logging.FileHandler("logfile.txt", "w", "utf-8")
-# info_handler.setLevel(logging.INFO)
-# info_handler.setFormatter(file_format)
-# log.addHandler(info_handler)
+info_handler = logging.FileHandler("logfile.txt", "w", "utf-8")
+info_handler.setLevel(logging.DEBUG)
+info_handler.setFormatter(file_format)
+log.addHandler(info_handler)
 
 error_handler = logging.FileHandler("errors.txt", "w", "utf-8")
 error_handler.setLevel(logging.ERROR)
@@ -32,7 +32,7 @@ log.addHandler(error_handler)
 # -----------------------------------------------------------------
 
 # rpcuser and rpcpassword are set in the bitcoin.conf file
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('turkyrpc', 'turkypass'))
+rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('turkyrpc', 'turkypass'), timeout=1200)
 
 best_block_hash = rpc_connection.getbestblockhash()
 best_block = rpc_connection.getblock(best_block_hash)
@@ -47,6 +47,7 @@ PROPERTY_NAME_LAST_BLOCK = "last_block"
 #                             functions
 # -----------------------------------------------------------------
 
+# iterate over a set of block transactions, retrieve the transaction data in batches, and process them
 def process_transactions(utxo_set, txids, block_hash):
     step = 100
     total_transactions = len(txids)
@@ -55,7 +56,11 @@ def process_transactions(utxo_set, txids, block_hash):
             tx_commands = [ [ "getrawtransaction", txids[i], True, block_hash ] for i in range(i, min(i + step, total_transactions)) ]
             transactions = rpc_connection.batch_(tx_commands)
         except JSONRPCException as err:
-            log.error(f"RPC Exception {err}")
+            log.error(f"RPC Exception {err} errno: {err.errno}\nwhile attempting to retrieve transactions from: {block_hash}")
+            return False
+        except IOError as err:
+            log.error(f"IO Error {err} errno: {err.errno}\nwhile attempting to retrieve transactions from: {block_hash}")
+            return False
         
         # process each transaction by removing the input utxos from the unspent set and adding the new outputs as unspent
         for transaction in transactions:
@@ -77,38 +82,60 @@ def process_transactions(utxo_set, txids, block_hash):
                 new_output = TxOutput(transaction["txid"], output["n"], output["scriptPubKey"])
                 log.info(f"adding new output with key: {new_output}")
                 utxo_set.add(new_output)
+    return True
+
+# iterate over a set of block hashes to retrieve each block individually and call process_transactions with all transaction hashes
+def process_blocks(utxo_set, block_hashes):
+    with alive_bar(len(block_hashes)) as progress_bar:
+        for block_hash in block_hashes:
+            try:
+                block = rpc_connection.getblock(block_hash)
+            except JSONRPCException as err:
+                log.error(f"RPC Exception {err} errno: {err.errno}\nwhile attempting to retrieve block: {block_hash}")
+                return False
+            except IOError as err:
+                log.error(f"IO Error {err} errno: {err.errno}\nwhile attempting to retrieve block: {block_hash}")
+                return False
+
+            current_block_height = block["height"]
+            block_hash = block["hash"]
+            txids = block["tx"]
+            log.info(f"block {current_block_height} with hash {block_hash} contains {len(txids)} transactions")
+            if not process_transactions(utxo_set, txids, block_hash):
+                return False
+            progress_bar()
+    return True
 
 def build_utxo_set(utxo_set, start_height, end_height = best_block_height):
     step = 1000
     last_block_processed = None
-    end_height = end_height + 1 # height is zero-indexed
-    with alive_bar(end_height) as progress_bar:
-        progress_bar(start_height)
-        for i in range(start_height, end_height, step):
-            commands = [ [ "getblockhash", height] for height in range(i, min(i + step, end_height)) ]
-            try:
-                block_hashes = rpc_connection.batch_(commands)
-                blocks = rpc_connection.batch_([ [ "getblock", h ] for h in block_hashes ])
-            except JSONRPCException as err:
-                log.error(f"RPC Exception {err}")
+    print(f"processing {end_height - start_height} blocks in the range [{start_height}, {end_height}]")
+    for i in range(start_height, end_height, step):
+        target_height = min(i + step, end_height)
+        commands = [ [ "getblockhash", height] for height in range(i, target_height) ]
+        try:
+            block_hashes = rpc_connection.batch_(commands)
+        except JSONRPCException as err:
+            log.error(f"RPC Exception {err}")
+            break
+        except IOError as err:
+            log.error(f"IO Error {err}")
+            break
 
-            log.info(f"{len(blocks)} blocks retrieved")
-            for block in blocks:
-                block_height = block["height"]
-                block_hash = block["hash"]
-                txids = block["tx"]
-                log.info(f"block {block_height} with hash {block_hash} contains {len(txids)} transactions")
-                process_transactions(utxo_set, txids, block_hash)
-                last_block_processed = block_height
-                progress_bar()
+        log.info(f"{len(block_hashes)} block hashes retrieved")
+        if process_blocks(utxo_set, block_hashes):
+            last_block_processed = target_height
+        else:
+            log.error(f"unable to process all blocks in current batch. [{i}, {target_height}]")
+            break
 
-            # save our progress every [step] blocks
-            save(utxo_set, last_block_processed)
-
+        # save our progress every [step] blocks
+        save(utxo_set, last_block_processed)
+        print(f"progress is saved at block {last_block_processed}!")
     return last_block_processed
 
 # writes the current utxo set and other stateful properties to files
-def save(utxo_set, last_block_processed, finished = False):
+def save(utxo_set, last_block_processed):
     with open(FILE_NAME_UTXO, "w", encoding="utf-8") as utxo_file:
         utxo_file.write(jsonpickle.encode(utxo_set))
 
@@ -117,17 +144,16 @@ def save(utxo_set, last_block_processed, finished = False):
     with open(FILE_NAME_CACHE, "w", encoding="utf-8") as cache_file:
         cache_file.write(json.dumps(cache))
 
-    if not finished:
-        print("progress is saved!")
-
 def load():
     utxo_set = None
     last_block_processed = None
 
     if os.path.exists(FILE_NAME_UTXO):
         with open(FILE_NAME_UTXO, "r", encoding="utf-8") as utxo_file:
-            # for the record, I really don't like this.
-            utxo_set = jsonpickle.decode(utxo_file.read())
+            print("loading UTXOs from file")
+            with alive_bar() as progress_bar:
+                # for the record, I really don't like this.
+                utxo_set = jsonpickle.decode(utxo_file.read())
 
     if os.path.exists(FILE_NAME_CACHE):
         with open(FILE_NAME_CACHE, "r", encoding="utf-8") as cache_file:
@@ -146,10 +172,10 @@ if __name__ == "__main__":
     start_height = last_block_processed + 1
     end_height = (TESTING and TESTING_HEIGHT) or best_block_height
 
-    log.debug(len(utxo_set))
-    log.debug(last_block_processed)
-    log.debug(start_height)
-    log.debug(end_height)
+    log.debug(f"utxo set size {len(utxo_set)}")
+    log.debug(f"last_block_processed: {last_block_processed}")
+    log.debug(f"start_height {start_height}")
+    log.debug(f"end_height {end_height}")
 
     last_block_processed = build_utxo_set(utxo_set, start_height, end_height) or last_block_processed
-    save(utxo_set, last_block_processed, True)
+    save(utxo_set, last_block_processed)
