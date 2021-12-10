5f9f75ba304c2b1c05362ca33e427152e733e3af
Jesse Frush <jesse.frush@gmail.com>
2021-12-10 12:00:38 -0800

    birthday_attack greatly simplified through the use of rpc_controller

--- birthday_attack.py
+++ birthday_attack.py
@@ -4,19 +4,12 @@ import logging
 import time
 from utxo.TXOutput import TXOutput, ScriptDecodingException
 from delayed_keyboard_interrupt import DelayedKeyboardInterrupt
-from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
+from rpc_controller.rpc_controller import RpcController
 from alive_progress import alive_bar
 
 # -----------------------------------------------------------------
 #                             globals
 # -----------------------------------------------------------------
-
-# rpcuser and rpcpassword are set in the bitcoin.conf file, and yes don't worry this is a dummy user and password
-rpc_connection = AuthServiceProxy("http://%s:%s@127.0.0.1:8332"%('turkyrpc', 'turkypass'), timeout=1200)
-
-best_block_hash = rpc_connection.getbestblockhash()
-best_block = rpc_connection.getblock(best_block_hash)
-best_block_height = int(best_block["height"])
 
 # file and output related constants
 FILE_NAME_CACHE = "cache.json"
@@ -92,105 +85,36 @@ def decode_transaction_scripts(transactions):
                 progress_bar()
 
 # iterate over a set of block transactions, retrieve the transaction data in batches, and process them
-def process_transactions(utxo_set, txids, block_hash):
-    step = 100
-    total_transactions = len(txids)
-    for i in range(0, total_transactions, step):
-        try:
-            tx_commands = [ [ "getrawtransaction", txids[i], True, block_hash ] for i in range(i, min(i + step, total_transactions)) ]
-            transactions = rpc_connection.batch_(tx_commands)
-        except JSONRPCException as err:
-            log.error(f"RPC Exception {err} errno: {err.errno}\nwhile attempting to retrieve transactions from: {block_hash}")
-            return False
-        except IOError as err:
-            log.error(f"IO Error {err} errno: {err.errno}\nwhile attempting to retrieve transactions from: {block_hash}")
-            return False
-        
-        # process each transaction by removing the input utxos from the unspent set and adding the new outputs as unspent
-        for transaction in transactions:
-            for input in transaction["vin"]:
+def process_transactions(rpc, utxo_set, txids, block_hash):
+    for txid in txids:
+        transaction = rpc.getrawtransaction(txid, True, block_hash)
+        for input in transaction["vin"]:
 
-                # coinbase transactions do not have input txids and cannot already exist in the utxo set
-                if input.get("coinbase"):
-                    break
+            # coinbase transactions do not have input txids and cannot already exist in the utxo set
+            if input.get("coinbase"):
+                break
 
-                spent_output = TXOutput(input["txid"], input["vout"])
-                if spent_output in utxo_set:
-                    log.info(f"removing spent output {spent_output}")
-                    utxo_set.remove(spent_output)
-                else:
-                    log.error(f"spent output not found: {spent_output}")
+            spent_output = TXOutput(input["txid"], input["vout"])
+            if spent_output in utxo_set:
+                log.info(f"removing spent output {spent_output}")
+                utxo_set.remove(spent_output)
+            else:
+                log.error(f"spent output not found: {spent_output}")
 
-            # add all outputs to utxo set
-            for output in transaction["vout"]:
-                new_output = TXOutput(transaction["txid"], output["n"], block_hash, output["scriptPubKey"]["hex"])
-                log.info(f"adding new output with key: {new_output}")
-                utxo_set.add(new_output)
+        # add all outputs to utxo set
+        for output in transaction["vout"]:
+            new_output = TXOutput(transaction["txid"], output["n"], block_hash, output["scriptPubKey"]["hex"])
+            log.info(f"adding new output with key: {new_output}")
+            utxo_set.add(new_output)
     return True
 
-# iterate over a set of block hashes to retrieve each block individually and call process_transactions with all transaction hashes
-def process_blocks(utxo_set, block_hashes):
-    with alive_bar(len(block_hashes)) as progress_bar:
-        for block_hash in block_hashes:
-            try:
-                block = rpc_connection.getblock(block_hash)
-            except JSONRPCException as err:
-                log.error(f"RPC Exception {err} errno: {err.errno}\nwhile attempting to retrieve block: {block_hash}")
-                return False
-            except IOError as err:
-                log.error(f"IO Error {err} errno: {err.errno}\nwhile attempting to retrieve block: {block_hash}")
-                return False
-
-            current_block_height = block["height"]
-            block_hash = block["hash"]
-            txids = block["tx"]
-            log.info(f"block {current_block_height} with hash {block_hash} contains {len(txids)} transactions")
-            if not process_transactions(utxo_set, txids, block_hash):
-                return False
-            progress_bar()
-    return True
-
-# using rpc, query parts of the blockchain in small groups until we are able to build the entire utxo set.
-# note that it would surely be faster and easier to read the data files directly. I am doing it this way
-# for fun and experimentation with Python, and to keep the node running and TODO: updating live as blocks are processed.
-def build_utxo_set(utxo_set, start_height, end_height = best_block_height):
-    step = 1000
-    last_block_processed = None
-    last_save_time = time.time()
-    MINUTES_BETWEEN_SAVES = 20.0
-    SECONDS_PER_MINUTE = 60.0
-    print(f"processing {end_height - start_height + 1} blocks in the range [{start_height}, {end_height}]")
-
-    # end_height is a zero-indexed value representing the height of the max block we want to process
-    # we will batch for [step] block hashes at once due to predictable size of 32,000 bytes.
-    # we will take care to include the end_height block by adding 1 but also not to go over the
-    # available blocks by a (max) full step during the last iteration.
-    end_height = end_height + 1
-    for i in range(start_height, end_height, step):
-        count = min(step, end_height - i)
-        commands = [ [ "getblockhash", height] for height in range(i, i + count) ]
-        try:
-            block_hashes = rpc_connection.batch_(commands)
-        except JSONRPCException as err:
-            log.error(f"RPC Exception {err}")
-            break
-        except IOError as err:
-            log.error(f"IO Error {err}")
-            break
-
-        log.info(f"{len(block_hashes)} block hashes retrieved")
-        if process_blocks(utxo_set, block_hashes):
-            last_block_processed = i + count - 1
-        else:
-            log.error(f"unable to process all blocks in current batch. [{i}, {i + count - 1}]")
-            break
-
-        # save our progress if enough time has passed
-        if (time.time() - last_save_time) > (MINUTES_BETWEEN_SAVES * SECONDS_PER_MINUTE):
-            save(utxo_set, last_block_processed)
-            print(f"progress is saved at block {last_block_processed}!")
-            last_save_time = time.time()
-    return last_block_processed
+def process_block(rpc, utxo_set, block_height):
+    block_hash = rpc.getblockhash(block_height)
+    block = rpc.getblock(block_hash)
+    txids = block["tx"]
+    log.info(f"block {block_height} with hash {block_hash} contains {len(txids)} transactions")
+    if process_transactions(rpc, utxo_set, txids, block_hash):
+        return block_height
 
 # -----------------------------------------------------------------
 #                        saving & loading
@@ -237,23 +161,48 @@ def load():
 #                             main
 # -----------------------------------------------------------------
 
-TESTING = False
+MINUTES_BETWEEN_SAVES = 20.0
+SECONDS_PER_MINUTE = 60.0
+STAY_BEHIND_BEST_BLOCK_OFFSET = 6
+
+def get_target_block_height(rpc):
+    best_block_hash = rpc.getbestblockhash()
+    best_block = rpc.getblock(best_block_hash)
+    best_block_height = int(best_block["height"])
+    target_block_height = (TESTING and TESTING_HEIGHT) or best_block_height - STAY_BEHIND_BEST_BLOCK_OFFSET
+    return target_block_height
+
+TESTING = True
 TESTING_HEIGHT = 1000
 if __name__ == "__main__":
     utxo_set, last_block_processed = load()
-
     utxo_set = utxo_set or set()
     last_block_processed = last_block_processed or 0
-    start_height = last_block_processed + 1
-    end_height = (TESTING and TESTING_HEIGHT) or best_block_height
+    last_save_time = time.time()
+    next_save_time = last_save_time + (MINUTES_BETWEEN_SAVES * SECONDS_PER_MINUTE)
+    rpc = RpcController()
 
-    log.debug(f"utxo set size {len(utxo_set)}")
-    log.debug(f"last_block_processed: {last_block_processed}")
-    log.debug(f"start_height {start_height}")
-    log.debug(f"end_height {end_height}")
-
-    if start_height < end_height:
-        last_block_processed = build_utxo_set(utxo_set, start_height, end_height) or last_block_processed
+    target_block_height = get_target_block_height(rpc)
+    running = True
+    while running:
+        while last_block_processed < target_block_height and time.time() < next_save_time:
+            try:
+                last_block_processed = process_block(rpc, utxo_set, last_block_processed + 1) or last_block_processed
+            except KeyboardInterrupt:
+                log.info(f"KeyboardInterrupt intercepted at {time.time()}")
+                print(f"Keyboard interrupt received, saving and stopping...")
+                running = False
+                break
+            
         save(utxo_set, last_block_processed)
+        last_save_time = time.time()
+        next_save_time = last_save_time + (MINUTES_BETWEEN_SAVES * SECONDS_PER_MINUTE)
+        target_block_height = get_target_block_height(rpc)
 
-    decode_transaction_scripts(utxo_set)
+        # this is here to prevent infinite looping over the save functionality and is temporary.
+        # In the future, we will spend the remaining time trying to generate collisions with
+        # the utxo set and so the loop is designed with that in mind.
+        if last_block_processed >= target_block_height:
+            running = False
+
+    # decode_transaction_scripts(utxo_set)
